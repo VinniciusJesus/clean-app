7d824dd3be70c6087b338f455a60f2562bb19eae
Marcos Vinicius <viniciusdejesus565@gmail.com>
2020-09-20 12:06:32 -0300

    move production code to their own target

---
+++ lib/data/usecases/remote_authentication.dart
@@ -0,0 +1,23 @@
+import 'package:meta/meta.dart';
+
+import '../../domain/usecases/usescases.dart';
+
+import '../http/http_client.dart';
+
+class RemoteAuthentication {
+  final HttpClient httpClient;
+  final String url;
+
+  RemoteAuthentication({
+    @required this.httpClient,
+    @required this.url,
+  });
+
+  Future<void> auth(AuthenticationParams params) async {
+    await httpClient.request(
+      url: url,
+      method: 'post',
+      body: params.toJson(),
+    );
+  }
+}
