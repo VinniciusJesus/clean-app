3134d8398ab75ea5f526d1d23c2447eebabc13e2
Marcos Vinicius <viniciusdejesus565@gmail.com>
2020-09-23 19:00:07 -0300

    ensure HttpAdapter calls post with correct Headers

---
+++ test/infra/http/http_adapter_test.dart
@@ -0,0 +1,56 @@
+import 'package:faker/faker.dart';
+import 'package:http/http.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+
+class HttpAdapter {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<void> request({
+    @required String url,
+    @required String method,
+    Map body,
+  }) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accepte': 'application/json',
+    };
+    await client.post(
+      url,
+      headers: headers,
+    );
+  }
+}
+
+class ClientSpy extends Mock implements Client {}
+
+void main() {
+  group(
+    'post',
+    () {
+      test(
+        'Should call post with correct values',
+        () async {
+          final client = ClientSpy();
+          final sut = HttpAdapter(client);
+          final url = faker.internet.httpUrl();
+
+          await sut.request(url: url, method: 'post');
+
+          verify(
+            client.post(
+              url,
+              headers: {
+                'content-type': 'application/json',
+                'accepte': 'application/json',
+              },
+            ),
+          );
+        },
+      );
+    },
+  );
+}
