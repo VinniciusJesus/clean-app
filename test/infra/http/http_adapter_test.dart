3ca4c87a4ef929af59d3dd7e18fdb06191b3a8b9
Marcos Vinicius <viniciusdejesus565@gmail.com>
2020-09-23 19:16:23 -0300

    ensure HttpAdapter calls post without  body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -20,11 +20,8 @@ class HttpAdapter {
       'content-type': 'application/json',
       'accepte': 'application/json',
     };
-    await client.post(
-      url,
-      headers: headers,
-      body: jsonEncode(body)
-    );
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    await client.post(url, headers: headers, body: jsonBody);
   }
 }
 
@@ -56,6 +53,17 @@ void main() {
                   'accepte': 'application/json',
                 },
                 body: '{"any_key":"any_value"}'),
+          );
+        },
+      );
+
+      test(
+        'Should call post without body',
+        () async {
+          await sut.request(url: url, method: 'post');
+
+          verify(
+            client.post(any, headers: anyNamed('headers')),
           );
         },
       );
