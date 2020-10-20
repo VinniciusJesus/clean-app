508d7dd1ebe04533d01911c09f94d729d07be20c
Marcos Vinicius <viniciusdejesus565@gmail.com>
2020-10-20 17:24:30 -0300

    Conclusao aula 21

---
+++ lib/main/presention/presenters/stream_login_presenter.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+import 'package:curso_manguinho/main/protocols/protocols.dart';
+import 'package:meta/meta.dart';
+
+class StreamLoginPresenter {
+  final Validation validation;
+  final _emailErrorController = StreamController<String>();
+  final _passwordErrorController = StreamController<String>();
+  final _isFormValidController = StreamController<bool>();
+  String _email;
+  String _password;
+  String _emailError;
+  String _passwordError;
+
+  Stream<String> get emailErrorStream =>
+      _emailErrorController.stream.distinct();
+  Stream<String> get passwordErrorStream =>
+      _passwordErrorController.stream.distinct();
+  Stream<bool> get isFormValidStream =>
+      _isFormValidController.stream.distinct();
+
+  StreamLoginPresenter({@required this.validation});
+
+  void _validateForm() {
+    _isFormValidController.add(_emailError == null &&
+        _email != null &&
+        _passwordError == null &&
+        _password != null);
+  }
+
+  void validateEmail(String email) {
+    _email = email;
+    _emailError = validation.validate(field: 'email', value: email);
+    _emailErrorController.add(_emailError);
+    _validateForm();
+  }
+
+  void validatePassword(String password) {
+    _password = password;
+    _passwordError = validation.validate(field: 'password', value: password);
+    _passwordErrorController.add(_passwordError);
+    _validateForm();
+  }
+}
